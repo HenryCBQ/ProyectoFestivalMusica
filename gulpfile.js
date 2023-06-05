ff9499a26735ab770f77a6ec268110b47b4c1c23
henry <henryalexis1999@gmail.com>
2023-06-05 08:25:16 -0500

    Creación de archivos scss para administrar los estilos de manera separada, se crea automatización en gulp para guardar y compilar los estilos escritos en scss a css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
-//Creo una funcion de JavaScript y le paso un collback (funcion que se ejecuta antes)
-function tarea(done){
-    console.log('Mi primera tarea con gulp');
+//Extraer funciones del node_modules para compilar el código SASS a CSS, puntualmente src es para ubicar archivos y el dest para guardar
+//watch permite ejecutar tareas cada vez que se actualiza la hoja de estilos de SCSS
+const {src, dest, watch} = require("gulp");
+const sass = require("gulp-sass")(require("sass")); //Llamar el archivo SASS y el archivo gulp-sass que permite comunicacion entre ambos
+
+//Creo la funcion para leer, compilñar y guardar la hoja de estilos.
+function css(done){
+    src("src/scss/**/*.scss") //Identidico todos los archivo con extension SCSS de la carpeta scss
+        .pipe(sass()) //El pipe ejecuta una tarea secuencialmente, entonces ejecuta el archivo sass mandado a llamar con require anteriormente
+        .pipe(dest("build/css")); //Guarda el archivo
+    done(); //Indico dónde termina la función
+}
+//Función para compilar el archivo app.scss cad avez que se modifique
+function dev(done){
+    watch("src/scss/**/*.scss", css); //Indico ruta del archivo y llamo la función que lo compila
     done();
 }
 
 //Node tiene una manera diferente de llamar una funcion
-//Comando para ejecutar la funcion npx gulp primeraTarea
-exports.tarea = tarea; //No es necesario ponerle parentesis a la funcion para mandarla a llamar con node
+//Comando para ejecutar funciones en gulp -> npx gulp nombreFuncion
+//Con control+c pauso la funcion de watch
+exports.css = css; //No es necesario ponerle parentesis a la funcion para mandarla a llamar con node
+exports.dev = dev;
