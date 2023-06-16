d4d765cbec278b469a87770fa58ba8cf1fba4274
henry <henryalexis1999@gmail.com>
2023-06-16 08:47:21 -0500

    Se finaliza los estilos y JS de la sección de galería, se crea estructura html y se le da estilos a la seccíón de boletos y se crea el html y se da estilos al footer

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,40 @@ function crearGaleria(){
             <img src="build/img/thumb/${i}.jpg" alt="Imagen Vocalista">
         `;
 
+        imagen.onclick = function(){ //Creo funcion para el evento clic
+            mostrarImagen(i); //Envio indice de la imagen al cual le doy clic
+        }
         galeria.appendChild(imagen);
     }
 }
 
+//Mostrar la version grande de la imagen cuando se da clic sobre una imagen de la galeria
+function mostrarImagen(id){ //Recibe el id de la imagen al cual se esta dando clic
+    const imagen = document.createElement('picture');
+        //Se crea un picture con las imagenes grandes
+        imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="image/avif">
+            <source srcset="build/img/grande/${id}.webp" type="image/webp">
+            <img src="build/img/grande/${id}.jpg" alt="Imagen Vocalista">
+        `;
+    const overlay = document.createElement('DIV'); //Creo un contenedor para la imagen
+    overlay.appendChild(imagen); //Imprimo la imagen dentro del div
+    overlay.classList.add('overlay');
+
+    //Boton para cerrar imagen
+    const btnCerrarImagen = document.createElement('P');
+    btnCerrarImagen.textContent ='X';
+    btnCerrarImagen.classList.add('btn-cerrar');
+    //Eliminar el overlay que contiene la imagen cuando se de clic en la X
+    btnCerrarImagen.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body'); //Elimino la clase scss para que el body no quede fijado una vez cierro el overlay
+        overlay.remove();
+    }
+    overlay.appendChild(btnCerrarImagen); //Lo agrego al html en el overlay
+
+    //Mostrar el bloque creado con la imagen en todo el body del documento html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body'); //Agregar clase para fijar el overlay
+}
